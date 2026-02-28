283b6683804ff485752e04e67339195322b80778
IsoardIMarius <misoardi13100@outlook.fr>
2026-03-01 00:50:04 +0100

    init

--- internal/playerdata/store.go
+++ internal/playerdata/store.go
@@ -14,7 +14,7 @@ type Store struct {
 func NewStore() *Store {
 	return &Store{
 		data: PlayerData{
-			PlayerID: "p3",
+			PlayerID: "p4",
 			Username: "jammer",
 			Level:    1,
 			Region:   "eu-west",
