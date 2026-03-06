463cf0d9fc8f36e6731473b3f2eab1ed8613049d
Sylvain <sylvain@opusmajor.io>
2026-03-06 16:08:21 +0100

    test build

--- app/internal/playerdata/store.go
+++ app/internal/playerdata/store.go
@@ -14,7 +14,7 @@ type Store struct {
 func NewStore() *Store {
 	return &Store{
 		data: PlayerData{
-			PlayerID: "P1",
+			PlayerID: "P1-updated",
 			Username: "jammer",
 			Level:    1,
 			Region:   "eu-west",
